80e2ee13b7f3b4af92c2b9998733d58766c0a3b1
Naresh <nareshsaharan2016@gmail.com>
2019-08-19 17:00:26 +0530

    -

---
+++ a.js
@@ -0,0 +1,20 @@
+
+function openList1() {
+    var list = document.getElementById("ollist");
+
+    if (list.style.display == "none"){
+        list.style.display = "block";
+    }else{
+        list.style.display = "none";
+    }
+}
+
+function openList2() {
+    var list = document.getElementById("ollist2");
+
+    if (list.style.display == "none"){
+        list.style.display = "block";
+    }else{
+        list.style.display = "none";
+    }
+}
